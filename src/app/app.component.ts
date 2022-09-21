858ecb98a46f6a8f0c01a585f93a0408cb6f7560
NourhanMA1 <102152175+NourhanMA1@users.noreply.github.com>
2022-09-21 20:34:44 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'GameMap';
+
+  counter(i: number) {
+    return new Array(i);
+  }
+}
